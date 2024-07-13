4834c617762031a1751ca3f56a7c8a978b363f07
aravind452 <aravindaru452@gmail.com>
2024-07-13 12:39:41 +0530

    contact section added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,13 @@ const Navbar = () => {
         <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a href="https://www.linkedin.com/in/aravindj2002/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/aravind452">
+          <FaGithub />
+        </a>
+        
       </div>
     </nav>
   );
